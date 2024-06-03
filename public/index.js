34b8abc2452c5cadf9efaa8d2de55e9ac73e36f6
Andrey <am.xcvi@gmail.com>
2024-06-03 19:14:36 +0300

    oooo

--- public/index.js
+++ public/index.js
@@ -5,37 +5,35 @@ window.onload = () => {
     "If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected."
   );
 
-  const el = document.getElementById("gps-camera-id");
+  const el = document.querySelector("[gps-new-camera]");
 
-  if (!el) {
-    const posDisplay = document.getElementById("position-display");
+  el.addEventListener("gps-camera-update-position", (e) => {
+    if (!testEntitiesAdded) {
+      alert(
+        `Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
+      );
 
-    posDisplay.textContent = "NOT FOUND CAMERA";
-  } else {
-    el.addEventListener("gps-camera-update-position", (e) => {
-      // eslint-disable-next-line no-constant-condition
-      if (true) {
-        alert(
-          `Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
-        );
+      const entity = document.getElementById("rabbit-entity");
+      const posDisplay = document.getElementById("position-display");
 
-        const entity = document.getElementById("rabbit-entity");
-        const posDisplay = document.getElementById("position-display");
+      if (entity && posDisplay) {
+        entity.setAttribute("gps-entity-place", {
+          latitude: e.detail.position.latitude + 0.0001,
+          longitude: e.detail.position.longitude + 0.0001,
+        });
 
-        if (entity && posDisplay) {
-          entity.setAttribute("gps-entity-place", {
-            latitude: e.detail.position.latitude + 0.0001,
-            longitude: e.detail.position.longitude + 0.0001,
-          });
+        posDisplay.textContent = JSON.stringify({
+          latitude: e.detail.position.latitude + 0.0001,
+          longitude: e.detail.position.longitude + 0.0001,
+        });
 
-          posDisplay.textContent = JSON.stringify({
-            latitude: e.detail.position.latitude + 0.0001,
-            longitude: e.detail.position.longitude + 0.0001,
-          });
+        el.setAttribute("gps-new-camera", {
+          simulateLatitude: e.detail.position.latitude,
+          simulateLongitude: e.detail.position.longitude,
+        });
 
-          testEntitiesAdded = true;
-        }
+        testEntitiesAdded = true;
       }
-    });
-  }
+    }
+  });
 };
