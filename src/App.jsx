d41b1dfe8521fc9d6cc9dd8ef61583096f13771f
Andrey <am.xcvi@gmail.com>
2024-06-03 13:26:38 +0300

    add react

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import "aframe";
+import { useEffect, useState } from "react";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) =>
+        setCurrentLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }),
+      (err) => console.log(err)
+    );
+  }, []);
+
+  return (
+    <a-scene
+      embedded
+      arjs
+      renderer="logarithmicDepthBuffer: true;"
+      vr-mode-ui="enabled: false"
+      gesture-detector
+      id="scene"
+    >
+      <a-marker
+        type="pattern"
+        preset="custom"
+        url="pattern/ifgi-pattern.patt"
+        id="ifgi-marker"
+      ></a-marker>
+      <a-entity
+        position="0 0 0"
+        scale="0.5 0.5 0.5"
+        gps-entity-place="latitude: <lat>; longitude: <lon>;"
+        gltf-model="/rabbit.glb"
+      ></a-entity>
+      <a-camera
+        gps-camera={`simulateLatitude: ${currentLocation.latitude}; simulateLongitude: ${currentLocation.longitude};`}
+        rotation-reader
+        id="camera"
+      />
+    </a-scene>
+  );
+}
+
+export default App;
