eb3de5e45d24fccd85a263dd9b1bcd6bf35d959d
Andrey <am.xcvi@gmail.com>
2024-06-03 19:08:20 +0300

    dd

--- public/index.js
+++ public/index.js
@@ -11,30 +11,31 @@ window.onload = () => {
     const posDisplay = document.getElementById("position-display");
 
     posDisplay.textContent = "NOT FOUND CAMERA";
-  }
+  } else {
+    el.addEventListener("gps-camera-update-position", (e) => {
+      // eslint-disable-next-line no-constant-condition
+      if (true) {
+        alert(
+          `Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
+        );
 
-  el.addEventListener("gps-camera-update-position", (e) => {
-    if (!testEntitiesAdded) {
-      alert(
-        `Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
-      );
+        const entity = document.getElementById("rabbit-entity");
+        const posDisplay = document.getElementById("position-display");
 
-      const entity = document.getElementById("rabbit-entity");
-      const posDisplay = document.getElementById("position-display");
+        if (entity && posDisplay) {
+          entity.setAttribute("gps-entity-place", {
+            latitude: e.detail.position.latitude + 0.0001,
+            longitude: e.detail.position.longitude + 0.0001,
+          });
 
-      if (entity && posDisplay) {
-        entity.setAttribute("gps-entity-place", {
-          latitude: e.detail.position.latitude + 0.0001,
-          longitude: e.detail.position.longitude + 0.0001,
-        });
+          posDisplay.textContent = JSON.stringify({
+            latitude: e.detail.position.latitude + 0.0001,
+            longitude: e.detail.position.longitude + 0.0001,
+          });
 
-        posDisplay.textContent = JSON.stringify({
-          latitude: e.detail.position.latitude + 0.0001,
-          longitude: e.detail.position.longitude + 0.0001,
-        });
-
-        testEntitiesAdded = true;
+          testEntitiesAdded = true;
+        }
       }
-    }
-  });
+    });
+  }
 };
